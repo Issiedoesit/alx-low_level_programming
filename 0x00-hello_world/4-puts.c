877dcfd1c90db885e112b6983c92f5e24d7dcec3
“issiedoesit” <isyekwe@gmail.com>
2022-09-08 15:10:14 -0700

    using puts to print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
+
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
-
-/*
- * void means main function accepts no input
- * and the function returns zero 
- *  - because the type is int 
- *  - and when it's able to print successfully
- */
