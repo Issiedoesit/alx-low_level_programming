d3eeb1cb86d3f60fe096459f58ae86618f7e24d5
“issiedoesit” <isyekwe@gmail.com>
2022-09-09 12:18:14 -0700

    base ten numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints all single digits number of base 10
+ * putchar used only twice
+ * returns 0
+ */
+
+
+int main (void)
+{
+	int n;
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0)
+}
