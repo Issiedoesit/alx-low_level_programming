ee9ae7723e37ce3a047ad2455cd672297869a27a
“issiedoesit” <isyekwe@gmail.com>
2022-09-13 15:19:37 -0700

    print alphabet ten newline fix two

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints the alphabet ten times, in lowercase,then new line
+ * print_alphabet_x10 - prints alphabet ten times, in lowercase,then new line
  */
 
 void print_alphabet_x10(void)
