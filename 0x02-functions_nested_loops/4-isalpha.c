ed69d0481fac974433baf5021966da6ef6e2b585
“issiedoesit” <isyekwe@gmail.com>
2022-09-13 15:34:57 -0700

    check if letter fix

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,8 +5,7 @@
  * @c: the character to be checked
  * Return: 1 if c is a letter, 0 otherwise
  */
-
 int _isalpha(int c)
 {
-	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')); 
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
